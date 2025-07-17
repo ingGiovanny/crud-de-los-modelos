bc407992fb409d052f301d20c2a894c7e58bb89e
Giovanny sanguino <giovannygps05@gmail.com>
2025-07-17 10:53:26 -0500

    modulo de facturacion realizado 2/3 Giovanny

--- Django_Crud_DigitSoft/proyecto con interfaz original - copia/config/apl/forms.py
+++ Django_Crud_DigitSoft/proyecto con interfaz original - copia/config/apl/forms.py
@@ -1,5 +1,5 @@
 from django.forms import *
-from .models import Administrador
+from .models import *
 
 class AdministradorForm(ModelForm):
     def __init__(self, *args, **kwargs):
@@ -19,6 +19,30 @@ class AdministradorForm(ModelForm):
                 }
             ),
             'descripcion' : Textarea(
+               attrs={
+                  'placeholder' : 'Ingrese una descripción',
+                  'rows': 3,
+                  'cols': 40,
+                }
+            ),
+        }
+        
+        
+class Facturacionform(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['fecha_id_venta'].widget.attrs['autofocus'] = True
+        
+    class Meta:
+        model = Facturacion
+        fields = '__all__'
+        widgets = {
+            'fecha_id_venta' : TextInput(
+                attrs={
+                    'placeholder' : 'Ingrese fecha en formato DD/MM/AAAA',
+                }
+            ),
+            'descripcion_venta' : Textarea(
                attrs={
                   'placeholder' : 'Ingrese una descripción',
                   'rows': 3,
--- Django_Crud_DigitSoft/proyecto con interfaz original - copia/config/apl/urls.py
+++ Django_Crud_DigitSoft/proyecto con interfaz original - copia/config/apl/urls.py
@@ -10,10 +10,15 @@ app_name = 'apl'
 urlpatterns = [
     path('base/', base_html, name='base'),
     path('plantilla/', plantilla_html, name='plantilla'),
-    path('listar/', AdministradorListView.as_view() , name='administrador_listar'),
+    path('administrador/listar/', AdministradorListView.as_view() , name='administrador_listar'),
     path('administrador/crear/', AdministradorCreateView.as_view(), name='administrador_crear'),
     path('administrador/editar/<int:pk>/', AdministradorUpdateView.as_view(), name='administrador_editar'),
     path('administrador/eliminar/<int:pk>/', AdministradorDeleteView.as_view(), name='administrador_eliminar'),
+    #url de facturacion
+    path('facturacion/listar/', FacturacionListview.as_view() , name='facturacion_listar'),
+    path('factuacion/crear/', FacturacionCreateView.as_view(), name='facturacion_crear'),
+    path('facturacion/editar/<int:pk>/', FacturacionUpdateView.as_view(), name='facturacion_editar'),
+    path('facturacion/eliminar/<int:pk>/', FacturacionDeleteView.as_view(), name='facturacion_eliminar'),
 
 ]
 
